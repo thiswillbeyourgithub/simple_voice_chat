2186820b4dd0bdc3f24ad45fcfda21e766ab5437
thiswillbeyourgithub <26625900+thiswillbeyourgithub@users.noreply.github.com>
2025-04-25 19:50:21 +0200

    minor
    Signed-off-by: thiswillbeyourgithub <26625900+thiswillbeyourgithub@users.noreply.github.com>

--- simple_voice_chat/__main__.py
+++ simple_voice_chat/__main__.py
@@ -7,7 +7,4 @@ import sys
 from .simple_voice_chat import main
 
 if __name__ == "__main__":
-    # Optionally, you could handle command-line arguments specifically for
-    # module execution here, but typically it's cleaner to let the main()
-    # function handle argparse as it already does.
     sys.exit(main())
